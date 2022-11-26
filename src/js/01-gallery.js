ff31d5592a29d2a70a28cec628d304307e9defc8
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-11-26 18:28:31 +0200

    completed first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    el => `<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: ['alt'],
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
