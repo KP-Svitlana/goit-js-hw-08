d331e9587a6978ab402f1473097c9560cc0849cd
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-11-26 21:27:38 +0200

    added second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(getLocalStorageTime, 1000));
+
+function getLocalStorageTime(data) {
+  localStorage.setItem(CURRENT_TIME, data.seconds);
+}
+
+let localStorageTime = localStorage.getItem(CURRENT_TIME);
+
+player.setCurrentTime(localStorageTime);
